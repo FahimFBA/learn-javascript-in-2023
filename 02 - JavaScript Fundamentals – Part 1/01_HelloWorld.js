4972ef266477e1d2f69ec64fcf7dab5a6454f4d5
FahimFBA <fahimbinamin@gmail.com>
2022-12-23 00:26:21 +0600

    Create 01_HelloWorld.js

---
+++ 02 - JavaScript Fundamentals – Part 1/01_HelloWorld.js
@@ -0,0 +1,10 @@
+// console.log("Hello World")
+// alert("Hello World")
+
+let js = "amazing";
+if (js === "amazing") alert("JavaScript is FUN!");
+js = 'boring'
+if (js === "amazing") alert("JavaScript is FUN!");
+
+console.log("Fahim")
+48+8+23-10
