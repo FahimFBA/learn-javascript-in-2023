9bfd33d78bf6bf60f90ba040393494291f712a4a
FahimFBA <fahimbinamin@gmail.com>
2023-01-18 00:09:09 +0600

    Operator Precedence: finished

---
+++ 02 - JavaScript Fundamentals – Part 1/Operator_Precedence/script.js
@@ -0,0 +1,62 @@
+let js = "amazing";
+
+console.log(40 + 8 + 23 - 10);
+
+console.log("Fahim");
+console.log(23);
+console.log(35);
+
+let firstName = "Fahim";
+console.log(firstName);
+
+let lastName = "Amin";
+console.log(lastName);
+
+let dogName = "tommy";
+console.log(dogName);
+
+let anotherName = "Matilda";
+console.log(anotherName);
+
+// camelcase - fahimBinAmin / firstNamePerson = standard in js
+
+let first_name_person = "Ruby";
+console.log(first_name_person);
+
+// let 3year = 3; variable name can't start with any digit
+
+// let fahim&amin = "FA"; can't use & in variable name
+// letter, number, underscore, dollar sign can be used in variable name
+
+// let function = 27; can't use the reserved keyword
+
+// let $function = 54;
+// let name = "Amin"; // don't use this
+
+// let PersonName = "Fahim"; // don't use this as that is not the convention
+// let personName = "Fahim"; // alright
+
+// let PI = 3.1415; // constant variable all in uppercase
+
+// let myFirstJob = "Teacher";
+// let myCurrentJob = "Programmer";
+
+// let job1 = "Teacher";
+// let job2 = "Programmer";
+
+// console.log(myFirstJob);
+
+const now = 2037;
+const ageFahim = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+console.log(25 - 10 - 5);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = (ageFahim + ageSarah) / 2;
+console.log(ageFahim, ageSarah, averageAge);
