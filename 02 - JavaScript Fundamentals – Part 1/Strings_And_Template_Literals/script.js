7ab0fe7b37963209efe6c87a6ff854462e5098dc
FahimFBA <fahimbinamin@gmail.com>
2023-02-09 21:24:52 +0600

    string and template literals finished

---
+++ 02 - JavaScript Fundamentals – Part 1/Strings_And_Template_Literals/script.js
@@ -0,0 +1,21 @@
+const firstName = "Fahim";
+const job = "Student";
+const birthYear = 1000;
+const year = 2023;
+const fahim =
+  "I'm " + firstName + ", a " + (year - birthYear) + " years old " + job + "!";
+console.log(fahim);
+
+const fahimNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+
+console.log(fahimNew);
+
+console.log(`Just a regular string...`);
+
+console.log(`String with \n\
+multiple \n\
+lines`);
+
+console.log(`String
+multiple
+lines`);
